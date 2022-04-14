9e3c7c1848f882a89d60ca9c925840cdbfe2e66f
OLAMIDE <olaniyilamie@gmail.com>
2022-04-14 15:17:17 +0100

    home and footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Nav from './components/Nav/Nav'
+import Home from './components/Home/Home';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 import './App.css';
 
 function App() {
   return (
     <>
       <Nav />
+      <Home/>
+      <Footer/>
     </>
   );
 }
